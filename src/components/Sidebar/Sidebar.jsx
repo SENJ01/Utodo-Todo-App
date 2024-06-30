79ec8e2bbab59a663f2592c07255f90f614dbd37
Senuvi Jayasinghe <senuvijayasinghe@gmail.com>
2024-06-30 21:50:34 +0530

    side bar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,44 +1,100 @@
 import React from 'react'
 import "../../styles/Sidebar.css";
-import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Row from 'react-bootstrap/Row';
-import Tab from 'react-bootstrap/Tab';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { UilFavorite } from '@iconscout/react-unicons'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-       <Tab.Container >
-      <Row>
-        <Col sm={12}>
-          <ListGroup >
-            <ListGroup.Item className='sidebar-fields' href="#link1">
-              Link 1
-            </ListGroup.Item>
-            <ListGroup.Item className='sidebar-fields' href="#link2">
-              Link 2
-            </ListGroup.Item>
-            <ListGroup.Item href="#link3">
-              Link 3
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
+      
+      <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button style={{backgroundColor:"#FC6736"}}>Search</Button>
+                </Form>
+      
+       <Navbar className="">
+        <Container>
+          <Navbar.Brand  style={{marginTop:"20px",color:"#fff"}} href="#">
+          <img
+              alt=""
+              src="./src/components/Sidebar/favorite.svg"
+              color="#FC6736"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            <a href="" style={{color:"#fff", marginLeft:'10px', textDecoration:"none"}}>Your Day</a>
+          </Navbar.Brand>
+          
+        </Container>
         
-      </Row>
-    </Tab.Container>
+      </Navbar>
+      <Navbar className="">
+        <Container>
+          <Navbar.Brand  style={{color:"#fff"}} href="#">
+          <img
+              alt=""
+              src="./src/components/Sidebar/favorite.svg"
+              color="#FC6736"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              
+            />{' '}
+            <a href="" style={{color:"#fff", marginLeft:'10px', textDecoration:"none"}}>Important</a>
+          </Navbar.Brand>
+          
+        </Container>
+        
+      </Navbar>
+      <Navbar className="">
+        <Container>
+          <Navbar.Brand  style={{ color:"#fff"}} href="#">
+          <img
+              alt=""
+              src="./src/components/Sidebar/favorite.svg"
+              color="#FC6736"
+              width="30"
+              height="30"
+              
+              className="d-inline-block align-top"
+            />{' '}
+            <a href="" style={{color:"#fff", marginLeft:'10px', textDecoration:"none"}}>All Tasks</a>
+          </Navbar.Brand>
+          
+        </Container>
+        
+      </Navbar>
+      <Navbar >
+        <Container className="logout">
+          <Navbar.Brand  style={{ color:"#fff"}} href="#">
+          <img
+              alt=""
+              src="./src/components/Sidebar/favorite.svg"
+              color="#FC6736"
+              width="30"
+              height="30"
+              
+              className="d-inline-block align-top"
+            />{' '}
+            <a href="" style={{color:"#fff", marginLeft:'10px', textDecoration:"none"}}>Logout</a>
+          </Navbar.Brand>
+          
+        </Container>
+        
+      </Navbar>
 
-    <Tab.Container >
-  
-        <Col sm={12}>
-          <ListGroup className="logout">
-            <ListGroup.Item  href="#link1">
-              Link 1
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-  
-    </Tab.Container>
     </div>
   )
 }
